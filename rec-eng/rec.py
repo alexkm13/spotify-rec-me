e839a8a70e6d4ea47c5a48cfc99e929f3cac533f
Alex Kim <alex@crc-dot1x-nat-10-239-226-95.bu.edu>
2023-04-01 15:44:41 -0400

    Added a folder for the recommendation engine.

---
+++ rec-eng/rec.py
@@ -0,0 +1,41 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from sklearn.neighbors import NearestNeighbors
+import pandas as pd
+import pull_id
+
+# authenticate with Spotify API
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_CLIENT_ID",
+                                               client_secret="YOUR_CLIENT_SECRET",
+                                               redirect_uri="YOUR_REDIRECT_URI",
+                                               scope="user-library-read"))
+# get user's saved tracks
+results = sp.current_user_saved_tracks()
+tracks = results['items']
+while results['next']:
+    results = sp.next(results)
+    tracks.extend(results['items'])
+
+# create dataframe of track features
+track_features = []
+for track in tracks:
+    features = sp.audio_features(track['track']['id'])[0]
+    features['id'] = track['track']['id']
+    features['name'] = track['track']['name']
+    features['artist'] = track['track']['artists'][0]['name']
+    track_features.append(features)
+df = pd.DataFrame(track_features)
+df = df.dropna()
+
+# fit Nearest Neighbors model
+nn = NearestNeighbors(metric='cosine', algorithm='brute')
+nn.fit(df.drop(['id', 'name', 'artist'], axis=1))
+
+# get recommendations for a song
+song_id = 'YOUR_SONG_ID'
+song_features = sp.audio_features(song_id)[0]
+recommendations = nn.kneighbors([song_features])[1][0]
+
+# display recommended songs
+for i in range(1, 6):
+    print(f"{i}. {df.iloc[recommendations[i]][['name', 'artist']].values[0]}")
